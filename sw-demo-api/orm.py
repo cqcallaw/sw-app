946584362610830090a7d50177d5857087eb5595
Caleb Callaway <cqcallaw@gmail.com>
2019-03-16 12:08:19 -0700

    More demo data

--- sw-demo-api/orm.py
+++ sw-demo-api/orm.py
@@ -43,9 +43,31 @@ def init_db(db_path, engine, session_maker):
     Base.metadata.create_all(engine)
 
     s = session_maker()
-    s.add(Role(id='admin', description='Administrators'))
-    s.add(Role(id='users', description='users'))
-    s.add(User(id='admin', name='The Administrator'))
-    s.add(User(id='user', name='A user'))
+    admin_role = Role(id='admin', description='Administrators')
+    user_role = Role(id='users', description='users')
+
+    admin_user = User(id='admin', name='The Administrator')
+    admin_user.roles.append(admin_role)
+    admin_user.roles.append(user_role)
+
+    plain_user = User(id='user', name='A user')
+    plain_user.roles.append(user_role)
+
+    alice = User(id='alice', name='Alice')
+    alice.roles.append(user_role)
+
+    bob = User(id='bob', name='Bob')
+    bob.roles.append(user_role)
+
+    eve = User(id='eve', name='Eve')
+    eve.roles.append(user_role)
+
+    s.add(admin_role)
+    s.add(user_role)
+    s.add(admin_user)
+    s.add(plain_user)
+    s.add(alice)
+    s.add(bob)
+    s.add(eve)
     s.commit()
 
