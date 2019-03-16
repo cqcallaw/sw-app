22bcb1a9c28afba24dfb59b4a2205ded441038c5
Caleb Callaway <cqcallaw@gmail.com>
2019-03-16 09:43:43 -0700

    More fully formed DB schema

--- demo.sql
+++ demo.sql
@@ -1,12 +1,38 @@
+DROP TABLE user_roles;
 DROP TABLE users;
+DROP TABLE roles;
 
-CREATE TABLE users (
-    user_id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL
+CREATE TABLE roles (
+    role_id TEXT PRIMARY KEY,
+    description TEXT
 );
 
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Bob');
-INSERT INTO users (name) VALUES ('Eve');
-INSERT INTO users (name) VALUES ('Frank');
-INSERT INTO users (name) VALUES ('George');
+CREATE TABLE users (
+    user_id TEXT PRIMARY KEY,
+    name TEXT
+);
+
+CREATE TABLE user_roles (
+    user_role_id INTEGER PRIMARY KEY,
+    user_id TEXT NOT NULL,
+    role_id TEXT NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES user(user_id)
+    FOREIGN KEY(role_id) REFERENCES role(role_id)
+);
+
+INSERT INTO roles (role_id, description) VALUES ('admin', 'Administrator Role');
+INSERT INTO roles (role_id, description) VALUES ('user', 'Users');
+
+INSERT INTO users (user_id, name) VALUES ('alice', 'Alice');
+INSERT INTO users (user_id, name) VALUES ('bob', 'Bob');
+INSERT INTO users (user_id, name) VALUES ('eve', 'Eve');
+INSERT INTO users (user_id, name) VALUES ('frank', 'Frank');
+INSERT INTO users (user_id, name) VALUES ('george', 'George');
+INSERT INTO users (user_id, name) VALUES ('admin', 'The Administrator');
+
+INSERT INTO user_roles(user_id, role_id) VALUES ('admin', 'admin');
+INSERT INTO user_roles(user_id, role_id) VALUES ('admin', 'users');
+INSERT INTO user_roles(user_id, role_id) VALUES ('alice', 'users');
+INSERT INTO user_roles(user_id, role_id) VALUES ('bob', 'users');
+INSERT INTO user_roles(user_id, role_id) VALUES ('eve', 'users');
+INSERT INTO user_roles(user_id, role_id) VALUES ('george', 'users');
