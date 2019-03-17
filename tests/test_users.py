0ce1fc8530bcd87c8aebb2588e4cecf430c870e9
Caleb Callaway <cqcallaw@gmail.com>
2019-03-17 14:49:11 -0700

    Test duplicate user creation

--- tests/test_users.py
+++ tests/test_users.py
@@ -32,6 +32,25 @@ class TestUser(BaseTestCase):
         self.assertIn('auth_token', response_data)
         self.assertIsNotNone(response_data['auth_token'])
 
+    def test_create_user_duplicate(self):
+        """ Test basic user POST """
+        data = {
+            'user_id': 'user',
+            'name': 'The New Guy',
+            'password': 'n',
+            'roles': [
+                {"role_id": "users"}
+            ]
+        }
+
+        response = self.client.post(
+            '/api/users',
+            data=json.dumps(data),
+            content_type='application/json',
+        )
+
+        self.assert400(response)
+
     def test_modify_user_no_login(self):
         """ Test basic user PATCH, no auth"""
         data = {'name': 'New Alice'}
