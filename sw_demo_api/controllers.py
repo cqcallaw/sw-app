18bce1d14db1e5422d2aba2f23cb5164d69cf473
Caleb Callaway <cqcallaw@gmail.com>
2019-03-16 23:55:21 -0700

    Stub of authentication logic

--- sw_demo_api/controllers.py
+++ sw_demo_api/controllers.py
@@ -1,4 +1,6 @@
 """ REST API Controllers """
+import datetime
+import jwt
 import flask_restless
 from sw_demo_api.models import Role, User
 from sw_demo_api.extensions import DATABASE_INSTANCE
@@ -18,18 +20,48 @@ def init(app):
             methods=['GET', 'PATCH', 'POST'],
             include_columns=['user_id', 'name', 'roles'],
             preprocessors={
-                'PATCH_SINGLE': [check_auth],
-                'PATCH_MANY': [check_auth_many]
+                'PATCH_SINGLE': [lambda instance_id, data, **kwargs: check_auth(app, instance_id, data, **kwargs)],
+                'PATCH_MANY': [lambda search_params, data, **kwargs: check_auth_many(app, search_params, data, **kwargs)]
             }
         )
 
         app.register_blueprint(role_api_blueprint)
         app.register_blueprint(user_api_blueprint)
 
-def check_auth_many(search_params=None, data=None, **kw):
+def check_auth_many(app, search_params=None, data=None, **kwargs):
     """ Check authentication status """
     raise flask_restless.ProcessingException(description='Not Authorized', code=401)
 
-def check_auth(instance_id=None, data=None, **kw):
+def check_auth(app, instance_id=None, data=None, **kw):
     """ Check authentication status """
     raise flask_restless.ProcessingException(description='Not Authorized', code=401)
+
+def encode_auth_token(app, user_id):
+    """
+    Generates the Auth Token
+    :return: string
+    """
+    payload = {
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
+        'iat': datetime.datetime.utcnow(),
+        'sub': user_id
+    }
+    return jwt.encode(
+        payload,
+        app.config['SECRET_KEY'],
+        algorithm='HS256'
+    )
+
+def decode_auth_token(app, auth_token):
+    """
+    Decodes the auth token
+    :param auth_token:
+    :return: integer|string
+    """
+    try:
+        payload = jwt.decode(auth_token, app.config['SECRET_KEY'])
+        return payload['sub']
+    except jwt.ExpiredSignatureError:
+        return 'Signature expired. Please log in again.'
+    except jwt.InvalidTokenError:
+        return 'Invalid token. Please log in again.'
