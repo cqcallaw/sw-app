e17e1ec7eaa7e38354a9cb6b2f04c2f216e0952d
Caleb Callaway <cqcallaw@gmail.com>
2019-03-19 07:58:53 -0700

    More web UI stubs

--- auth_demo/controllers.py
+++ auth_demo/controllers.py
@@ -5,6 +5,9 @@ def init(app):
     """ Init app controllers """
     with app.app_context():
         app.add_url_rule('/', view_func=base_handler, methods=['GET'])
+        app.add_url_rule('/login', view_func=login_handler, methods=['GET'])
+        app.add_url_rule('/logout', view_func=logout_handler, methods=['GET'])
+        app.add_url_rule('/register', view_func=register_handler, methods=['GET'])
         app.add_url_rule('/users', view_func=users_handler, methods=['GET'])
         app.add_url_rule('/users<string:user_id>', view_func=user_handler, methods=['GET'])
         app.add_url_rule('/register', view_func=register_handler, methods=['GET'])
@@ -13,11 +16,17 @@ def base_handler():
     """ Base URL handler """
     return render_template('index.html')
 
+def login_handler():
+    """ Login handler """
+
+def logout_handler():
+    """ Logout handler """
+
+def register_handler():
+    """ Register handler """
+
 def users_handler():
     """ Users view handler """
 
 def user_handler():
     """ User view handler """
-
-def register_handler():
-    """ Register view handler """
