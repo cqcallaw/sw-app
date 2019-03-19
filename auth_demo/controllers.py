e9bcc9ae3c824f07ac672b3bdad5f0e95fe28a7f
Caleb Callaway <cqcallaw@gmail.com>
2019-03-19 07:37:46 -0700

    More URL stubs

--- auth_demo/controllers.py
+++ auth_demo/controllers.py
@@ -5,7 +5,19 @@ def init(app):
     """ Init app controllers """
     with app.app_context():
         app.add_url_rule('/', view_func=base_handler, methods=['GET'])
+        app.add_url_rule('/users', view_func=users_handler, methods=['GET'])
+        app.add_url_rule('/users<string:user_id>', view_func=user_handler, methods=['GET'])
+        app.add_url_rule('/register', view_func=register_handler, methods=['GET'])
 
 def base_handler():
     """ Base URL handler """
     return render_template('index.html')
+
+def users_handler():
+    """ Users view handler """
+
+def user_handler():
+    """ User view handler """
+
+def register_handler():
+    """ Register view handler """
