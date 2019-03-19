4dc4625f485b58c7e6da1507e186074dc8d45eb7
Caleb Callaway <cqcallaw@gmail.com>
2019-03-18 23:02:25 -0700

    More flask-login boilerplate

--- auth_demo/controllers.py
+++ auth_demo/controllers.py
@@ -1,11 +1,14 @@
 """ App controllers """
-from flask import render_template
+import flask
+from flask_login import login_user, render_template
+from auth_demo.models import User
+from auth_demo.extensions import LOGIN_MANAGER
 
 def init(app):
     """ Init app controllers """
     with app.app_context():
         app.add_url_rule('/', view_func=base_handler, methods=['GET'])
-        app.add_url_rule('/login', view_func=login_handler, methods=['GET'])
+        app.add_url_rule('/login', view_func=login_handler, methods=['GET', 'POST'])
         app.add_url_rule('/logout', view_func=logout_handler, methods=['GET'])
         app.add_url_rule('/register', view_func=register_handler, methods=['GET'])
         app.add_url_rule('/users', view_func=users_handler, methods=['GET'])
@@ -17,7 +20,23 @@ def base_handler():
     return render_template('index.html')
 
 def login_handler():
-    """ Login handler """
+    """ Handle user login """
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Login and validate the user.
+        # user should be an instance of your `User` class
+        login_user(user)
+
+        flask.flash('Logged in successfully.')
+
+        redirect_url = flask.request.args.get('next')
+        # is_safe_url should check if the url is safe for redirects.
+        # See http://flask.pocoo.org/snippets/62/ for an example.
+        if not is_safe_url(redirect_url):
+            return flask.abort(400)
+
+        return flask.redirect(next or flask.url_for('index'))
+    return flask.render_template('login.html', form=form)
 
 def logout_handler():
     """ Logout handler """
@@ -30,3 +49,12 @@ def users_handler():
 
 def user_handler():
     """ User view handler """
+
+@LOGIN_MANAGER.user_loader
+def load_user(user_id):
+    """ Load user interface """
+    return User.query.filter_by(user_id=user_id).first()
+
+def is_safe_url(redirect_url):
+    """ Check for safe URL redirect """
+    return True
--- auth_demo/extensions.py
+++ auth_demo/extensions.py
@@ -1,11 +1,14 @@
 """ Extension instances """
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 DATABASE_INSTANCE = SQLAlchemy()
 BCRYPT_HANDLE = Bcrypt()
+LOGIN_MANAGER = LoginManager()
 
 def register_extensions(app):
     """ Register extensions with the Flask app """
     DATABASE_INSTANCE.init_app(app)
     BCRYPT_HANDLE.init_app(app)
+    LOGIN_MANAGER.init_app(app)
