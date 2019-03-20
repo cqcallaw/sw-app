eec2dd123c73ce9a1fa29c8fd65568ca2a5e67ad
Caleb Callaway <cqcallaw@gmail.com>
2019-03-20 07:04:02 -0700

    More login cleanup

--- auth_demo/forms.py
+++ auth_demo/forms.py
@@ -1,17 +1,15 @@
 """ SW auth app forms """
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField
 
 class LoginForm(FlaskForm):
     """ Login Form """
     user_id = StringField('Username')
     password = PasswordField('Password')
-    submit = SubmitField('Submit')
 
 class RegistrationForm(FlaskForm):
     """ Registration Form """
     user_id = StringField('Username')
     name = StringField('Username')
     password = PasswordField('Password')
-    submit = SubmitField('Submit')
