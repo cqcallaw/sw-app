89c1e48f3e8b668e20106d3160f904f8dae5fc8b
Caleb Callaway <cqcallaw@gmail.com>
2019-03-20 08:31:38 -0700

    Tidying up

--- connectivity-check/check-connectivity.sh
+++ connectivity-check/check-connectivity.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# check for at least one 'up' network interface
+# to run on startup, invoke from /etc/rc.local, create an inittab, or a @reboot cronjob
+
 count=$(ip link show up | grep 'link/' | grep -v 'link/loopback' | wc -l)
 
 if [ $count -lt 1 ]
